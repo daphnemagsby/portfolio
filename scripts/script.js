506f1914b2df0d7a4692839eb91ee0e14a23cfb8
daphnemagsby <daphnemagsby@gmail.com>
2021-08-06 01:53:47 -0500

    Add sticky header when scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 const toggleButton = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav');
 
-/*toggle hamburger menu and nav links*/
+//toggle hamburger menu and nav links
 toggleButton.addEventListener('click', function(){
   toggleButton.classList.toggle('is-active');
   navLinks.classList.toggle('active');
-})
+});
+
+//sticky header when srolling
+window.addEventListener('scroll', function(){
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY >0);
+});
